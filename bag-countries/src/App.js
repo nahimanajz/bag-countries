ad88628d093152b135a22fe4d99e06508ce40f62
nahimanajz <jazzonahimana@gmail.com>
2021-12-03 11:13:22 +0200

    init file

---
+++ bag-countries/src/App.js
@@ -0,0 +1,85 @@
+import React, {useState} from 'react'
+import { ArrowLeft } from './assets/ArrowLeft'
+import { Bell } from './assets/Bell'
+import { ToggleRight } from './assets/ToggleRight'
+import { ToggleLeft } from './assets/ToggleLeft'
+import { Sun } from './assets/Sun'
+import { Moon } from './assets/Moon'
+ 
+
+function App() {  
+  const [darkMode, setDarkMode] = useState(false)
+  const toggleMode =()=>setDarkMode(!darkMode)
+  return (
+      <div className="app">
+        <sidebar className="sidebar">
+           {/*  */}
+           <ul className="side-menu padding-32">
+             <li className="menu-item"> CHECKLIST</li>
+             <li className="menu-item"> Visited</li>
+             <li className="menu-item"> TO VISITE</li>
+           </ul>
+        </sidebar>
+        <main>
+          <div className="flex" style={{backgroundColor: 'red', minWidth: 375, margin:0,padding:0}}>
+              <div className="title-dark">Where in the world?</div>
+              <div className="flex">                
+                <span> Dark mode</span>                  
+                    <button onClick={toggleMode}> {darkMode ?<Sun/>:<Moon /> }</button>
+              </div>
+
+          </div>
+          <navbar className="flex-wrap">
+            <div className="flex mobile">              
+                <ArrowLeft />
+               <span className="toggle">Back</span>             
+            </div>
+            <div className="flex-end desktop">
+              <div className="flex">
+                  <span> Dark mode</span>                  
+                    <button onClick={toggleMode} className="toggle"> {darkMode ?<ToggleRight />:<ToggleLeft /> }</button>
+              <div className="circle"><Bell color={'#fff'} /> </div>
+              <div><b className="text-light">hey</b>, Jane</div>
+              <div className="circle" style={{backgroundImage:'url(./assets/man.png)'}}> </div>
+            </div>
+            </div>
+            
+          </navbar>
+           <div className="grid-2 margin-top-120">
+             <div>
+                  <img src="https://upload.wikimedia.org/wikipedia/en/thumb/b/ba/Flag_of_Germany.svg/1200px-Flag_of_Germany.svg.png" alt="Country flag"></img>
+             </div>
+             <div className="flex-col">
+               <div className="title-dark">Belgium</div>
+                <div className="grid-2">
+                  <ul>
+                    <li><b>Native Name:  </b> Belgie</li>
+                      <li><b>Population:  </b> 111.897.123</li>
+                      <li><b>Region:  </b> Europe</li>
+                      <li><b>Sub Region: </b>Western Europe</li>
+                      <li><b>Capital: </b>Brussels</li>
+                  </ul>
+                  <ul>
+                    <li><b>Top Level Domain:</b> .be</li>
+                      <li><b>Currencies:</b> 111.897.123</li>
+                      <li><b>Languages</b> Ducth, french, German</li> {/** TODO: map list of languages */}
+                  </ul>
+                </div>
+                <div className="container">
+                 <span><b>Border countries:</b></span>
+                  <span className="neighbor">France</span> 
+                  <span className="neighbor">Germany</span> <span className="neighbor">Netherlands</span>
+               </div>
+             </div>
+             
+           </div>
+        </main>
+        
+       
+        
+      </div>
+    );
+  
+}
+
+export default App;
